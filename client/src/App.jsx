988f1131958c3421acaf6a4755b0c916ab62bea5
indrashh <167687313+indrashh@users.noreply.github.com>
2025-04-14 21:29:01 +0700

    Learning how to Rendering Data with .map(),Importance of key in List Rendering,Fetching Data with useEffect and axios,Managing State with useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
-
-function Task(props) {
-  const { id, task, status } = props.todo;
-  const statusText = status ? "true" : "false";
-  return (
-    <>
-      <p>no: {id}</p>
-      <h1>Task: {task}</h1>
-      <h3>Status: {statusText}</h3>
-    </>
-  );
-}
+import Title from "./components/Title";
+import Table from "./components/Table";
+import Form from "./components/Form";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  function getTodos() {
-    axios({
-      method: "GET",
-      url: "http://localhost:3000/todos",
-    })
-      .then((result) => {
-        setTodos(result.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   return (
     <>
-      {todos.map((todo) => {
-        return <Task todo={todo} />;
-      })}
-      <button onClick={getTodos}>Klik</button>
+      <Title />
+      <Form />
+      <Table></Table>
     </>
   );
 }
